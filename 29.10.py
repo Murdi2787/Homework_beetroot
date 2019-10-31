80924808a516c8054ef16b8ac2049ea8ce2e8e19
Murdi2787 <aleks.shavlack@gmail.com>
2019-10-31 16:27:14 +0200

    hwork

--- 29.10.py
+++ 29.10.py
@@ -86,6 +86,22 @@ list_sort(L)
 # Задание №4
 # Написать функцию таймер, которая будет принимать функцию как аргумент и вычислять время выполнения этой функции.
 
+import timeit
+
+
+def list_s(arg1, arg2 ):
+    arg1 = [1, [12, 13], 'qweqeq', 3]
+    arg2 = ['213123', 2, [5, 6, 7, 7], 11]
+    return list_s(arg1, arg2)
+
+
+def speed(arg3):
+    tmr = timeit.Timer(arg3).repeat(1)
+    return tmr
+
+
+x = speed(list_s)
+print(x)
 
 # Задание №5
 # Создать функцию create_adder, внутри которой будет объявлена функция add_elems(list_of_elems) и будет возвращаться эта функция.
@@ -103,3 +119,60 @@ def create_adder(e):
 elem_lst = random.sample(range(1, 50), 3)
 print(elem_lst)
 create_adder(elem_lst)
+
+
+# Задание №6
+# Написать функцию create_counter, в которой будет объявлена переменная counter и функция generate_password,
+# вернуть функцию generate_password. Функция generate_password должна генерировать пароль и увеличивать counter на 1.
+# Вызвать функцию для генерации пароля несколько раз и посмотреть как изменится счетчик.
+
+
+# class Counter:
+#     def __init__(self, fnc):
+#         self.fnc= fnc
+#         self.count = count
+#
+#     def __call__(self, *args, **kwargs):
+#         self.count += 1
+#         return self.fnc(*args, **kwargs)
+
+def create_counter(e):
+    def gen_pswrd(e):
+        for n in range(nmbr):
+            pswrd = ''
+            for i in range(ln):
+                pswrd += random.choice(chars)
+                uppers = [l for l in pswrd if l.isupper]
+            print(pswrd)
+
+    return gen_pswrd(e)
+
+
+nmbr = int(input('Выберите кол-во паролей: '))
+ln = int(input('Выберите длинну пароля: '))
+chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+create_counter(chars)
+
+# Задание №7
+# Изменить пример make_averager, так чтоб функция хранила результат предыдущей суммы и количество елементов,
+# тогда зная эти два числа можно вычислить новое среднее значение. (используйте nonlocal)
+
+
+# Задание №8
+# С помощью функции map преобразовать случайного список целых чисел в новый список где каждый елеменет будет умножен на 2
+
+
+# Задание №9
+# С помощью функции filter выбрать только положительные целочисленные елементы случайного списка
+
+
+# Задание №10
+# С помощью функции reduce умножить все числа в списке
+
+
+# Задание №11
+# С помощью функции map преобразовать список строк в список чисел: [‘1’, ‘2’, ‘3’, ‘4’] => [1, 2, 3, 4]
+
+
+# Задание №12
+# С помощью функции map преобразовать список с милями в список с километрами, где 1 mile = 1.6 km
