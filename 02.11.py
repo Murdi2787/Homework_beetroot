0b6bc7f8844bf4a2ed60e1b2b0b7b8d403131913
Murdi2787 <aleks.shavlack@gmail.com>
2019-11-06 00:34:39 +0200

    hmwr

--- 02.11.py
+++ 02.11.py
@@ -122,6 +122,43 @@ obj_curr = USDcorrencyConverter(200, 'USD')
 obj_curr.convert = 350, 'EUR'
 print(obj_curr.convert)
 
+#---------------------------------------------------------------------------------------------------
+
+curr_inp = input('Введите валюту: ')
+val_inp = int(input('Введите сумму: '))
+
+
+class USDcurrencyConverter:
+
+    def __init__(self, value, currency):
+        self._value = value
+        self.currency = currency
+
+    @property
+    def convert(self):
+        find_curr = dict_curr[curr_inp]
+        return find_curr * val_inp
+
+    @convert.setter
+    def convert(self, currency):
+        self.currency = curr_inp
+        self._value = val_inp
+
+    @convert.deleter
+    def convert(self):
+        del self._value
+        del self.currency
+
+
+dict_curr = {
+    'USD': 24.72, 'EUR': 27.46, 'PLN': 6.44, 'RUB': 0.39,
+    'GBP': 31.89, 'CHF': 24.94, 'AUD': 17.08, 'HUF': 0.08,
+    'DKK': 3.68, 'CAD': 18.83, 'NOK': 2.71, 'CZK': 1.08,
+    'SEK': 2.57, 'JPY': 0.23,
+}
+obj_curr = USDcurrencyConverter(val_inp, curr_inp)
+print(obj_curr.convert)
+
 
 # Задание №5
 # Создать класс Карта с атрибутами значение и масть, перегрузить методы __lt__, __gt__, __eq__ для сравнения карт
